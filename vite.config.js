d8a1edaaa24b57ab11056ea5cd31fcf8ec38e6bd
Brian Grinstead <briangrinstead@gmail.com>
2023-03-06 21:55:13 -0800

    try to get build working

--- vite.config.js
+++ vite.config.js
@@ -5,21 +5,7 @@ export default defineConfig({
   plugins: [react()],
   base: "./",
   build: {
-    // https://github.com/vitejs/vite/issues/2433
-    // Attempting to fix errors like https://github.com/bgrins/data-ui-tests/actions/runs/4350881786/jobs/7602010504
-    // sourcemap: false,
-    // minify: false,
     rollupOptions: {
-      // cache: false,
-      // maxParallelFileOps: 2,
-      // output: {
-      //   sourcemap: false,
-      //   manualChunks: (id) => {
-      //    if (id.includes('node_modules')) {
-      //       return 'vendor';
-      //     }
-      //   },
-      // },
       input: {
         main: resolve(__dirname, "index.html"),
         charts: resolve(__dirname, "charts/index.html"),
